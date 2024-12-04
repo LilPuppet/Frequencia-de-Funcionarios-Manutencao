64cea5d8f01d6570f98852748891dd9361e34f4f
LilPuppet <laviniadantass@gmail.com>
2024-12-04 17:10:13 -0300

    Ta funcionano caraiu

--- frequencias/views.py
+++ frequencias/views.py
@@ -6,24 +6,3 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
-class FrequenciaListView(LoginRequiredMixin, ListView):
-    model = FrequenciaModel
-    template_name = "frequencias/frequencia_list.html"
-    context_object_name = "frequencias"
-
-class FrequenciaCreateView(LoginRequiredMixin, CreateView):
-    model = FrequenciaModel
-    template_name = "frequencias/frequencia_form.html"
-    fields = ['hora_inicio', 'hora_fim', 'funcionario']
-    success_url = reverse_lazy('frequencias:list')
-
-class FrequenciaUpdateView(LoginRequiredMixin, UpdateView):
-    model = FrequenciaModel
-    template_name = "frequencias/frequencia_form.html"
-    fields = ['hora_inicio', 'hora_fim', 'funcionario']
-    success_url = reverse_lazy('frequencias:list')
-
-class FrequenciaDeleteView(LoginRequiredMixin, DeleteView):
-    model = FrequenciaModel
-    template_name = "frequencias/frequencia_confirm_delete.html"
-    success_url = reverse_lazy('frequencias:list')
